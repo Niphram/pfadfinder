4dd156bb3729f4b64f2df159f4ac127fc67f2b5c
Niphram <theniphram@gmail.com>
2024-07-22 16:45:43 +0200

    Refactor

--- src/lib/preview/systems/index.ts
+++ src/lib/preview/systems/index.ts
@@ -6,3 +6,5 @@ export const SYSTEM_MAP = {
 	pathfinder: Pathfinder,
 	'kids-on-bikes': KidsOnBikes
 } satisfies Record<string, SystemData>;
+
+export type SystemName = keyof typeof SYSTEM_MAP;
--- src/routes/preview/+page.svelte
+++ src/routes/preview/+page.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
 	import { deleteCharacter, listCharacters, saveCharacter } from '$lib/preview/character-store';
-	import { KidsOnBikesCharacter } from '$lib/preview/systems/kids-on-bikes/character';
-	import { PathfinderCharacter } from '$lib/preview/systems/pathfinder/data/character';
+	import { SYSTEM_MAP, type SystemName } from '$lib/preview/systems';
 
 	let selectedSystem: string | undefined;
 
 	$: characterList = listCharacters(selectedSystem);
+
+	function createAndOpenCharacter(system: SystemName) {
+		saveCharacter(new SYSTEM_MAP[system].character()).then((id) =>
+			goto(`${base}/preview/character?id=${id}`)
+		);
+	}
+
+	async function deleteChar(id: string) {
+		await deleteCharacter(id);
+
+		characterList = listCharacters(selectedSystem);
+	}
 </script>
 
 <h1>Home</h1>
@@ -25,20 +37,17 @@
 			<a href="{base}/preview/character?id={char.id}"
 				>[{char.system}] {char.name} ({char.id}) ({char.updated.toLocaleString()})</a
 			>
-			<button on:click={() => deleteCharacter(char.id)} class="btn btn-warning btn-sm"
-				>Delete</button
-			>
+			<button on:click={() => deleteChar(char.id)} class="btn btn-warning btn-sm">Delete</button>
 		</li>
 	{/each}
 {/await}
 
-<button on:click={() => saveCharacter(new KidsOnBikesCharacter())}
-	>Add Kids on Bikes Character</button
+<button on:click={() => createAndOpenCharacter('kids-on-bikes')}>Add Kids on Bikes Character</button
 >
 
 <br />
 
-<button on:click={() => saveCharacter(new PathfinderCharacter())}>Add Pathfinder Char</button>
+<button on:click={() => createAndOpenCharacter('pathfinder')}>Add Pathfinder Char</button>
 
 <br />
 
