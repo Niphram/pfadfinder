53346af1c61403dcc357ce5f6bab2b1580e3b833
Niphram <theniphram@gmail.com>
2025-08-02 16:48:00 +0200

    proxy

--- src/lib/serde/proxy.ts
+++ src/lib/serde/proxy.ts
@@ -1,7 +1,18 @@
+import type { Ability } from '$lib/data';
+import type { Array } from './types/array.svelte';
+import type { Boolean } from './types/boolean.svelte';
+import type { Enumeration } from './types/enum.svelte';
 import type { Number } from './types/number.svelte';
+import type { Object } from './types/object.svelte';
 import type { String } from './types/string.svelte';
 
 export type SerdeProxy<T> =
 	T extends String<string, boolean> ? T['value']
 	: T extends Number<boolean> ? T['value']
-	: never;
+	: T extends Boolean ? T['value']
+	: T extends Enumeration<unknown, boolean> ? T['value']
+	: T extends Array<unknown> ? SerdeProxy<T['value']>[]
+	: T extends Object<object> ? { [K in keyof T['value']]: SerdeProxy<T['value'][K]> }
+	: T;
+
+type Test = SerdeProxy<Object<Ability>>;
