ba7ca3607666c8f6fc09dffc5158a5b44e05fa06
Niphram <theniphram@gmail.com>
2025-09-21 12:13:01 +0200

    enable loader again

--- src/lib/components/page-loader.svelte
+++ src/lib/components/page-loader.svelte
@@ -6,18 +6,14 @@
 	const { loading }: Props = $props();
 </script>
 
-{#if Math.random() < 0}
-	<div class="fixed top-0 right-0 z-[999] h-screen w-screen overflow-hidden">
-		<div
-			class={[
-				'h-full w-full',
-				'bg-transparent transition-opacity delay-300 duration-500',
-				'grid items-center justify-items-center',
-				'pointer-events-none invisible opacity-0',
-				loading && 'pointer-events-auto! visible! bg-black/40! opacity-100!',
-			]}
-		>
-			<span class="loading loading-bars w-32"></span>
-		</div>
-	</div>
-{/if}
+<div
+	class={[
+		'fixed top-0 right-0 z-[999] h-full max-h-screen! w-screen max-w-screen!',
+		'bg-transparent transition-opacity delay-300 duration-500',
+		'grid items-center justify-items-center',
+		'pointer-events-none invisible opacity-0',
+		loading && 'pointer-events-auto! visible! bg-black/40! opacity-100!',
+	]}
+>
+	<span class="loading loading-bars w-32"></span>
+</div>
