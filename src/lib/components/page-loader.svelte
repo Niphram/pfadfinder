ce89fbc5d31db902d8862bed049b989062bd23f5
Niphram <theniphram@gmail.com>
2025-09-21 11:51:53 +0200

    Fix layout again

--- src/lib/components/page-loader.svelte
+++ src/lib/components/page-loader.svelte
@@ -8,7 +8,7 @@
 
 <div
 	class={[
-		'fixed top-0 right-0 bottom-0 left-0 z-[999]',
+		'fixed top-0 right-0 z-[999] h-full w-full overflow-hidden',
 		'bg-transparent transition-opacity delay-300 duration-500',
 		'grid items-center justify-items-center',
 		'pointer-events-none invisible opacity-0',
