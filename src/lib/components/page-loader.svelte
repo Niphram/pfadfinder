fbdf61bc516103456f8ec90ceef3b2a0d1284ed8
Niphram <theniphram@gmail.com>
2025-09-21 12:05:15 +0200

    Experiment

--- src/lib/components/page-loader.svelte
+++ src/lib/components/page-loader.svelte
@@ -6,16 +6,18 @@
 	const { loading }: Props = $props();
 </script>
 
-<div class="fixed top-0 right-0 z-[999] h-screen w-screen overflow-hidden">
-	<div
-		class={[
-			'h-full w-full',
-			'bg-transparent transition-opacity delay-300 duration-500',
-			'grid items-center justify-items-center',
-			'pointer-events-none invisible opacity-0',
-			loading && 'pointer-events-auto! visible! bg-black/40! opacity-100!',
-		]}
-	>
-		<span class="loading loading-bars w-32"></span>
+{#if Math.random() < 0}
+	<div class="fixed top-0 right-0 z-[999] h-screen w-screen overflow-hidden">
+		<div
+			class={[
+				'h-full w-full',
+				'bg-transparent transition-opacity delay-300 duration-500',
+				'grid items-center justify-items-center',
+				'pointer-events-none invisible opacity-0',
+				loading && 'pointer-events-auto! visible! bg-black/40! opacity-100!',
+			]}
+		>
+			<span class="loading loading-bars w-32"></span>
+		</div>
 	</div>
-</div>
+{/if}
