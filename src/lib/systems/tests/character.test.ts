712a02d2329891a78955bd115747d3dbff73513b
Niphram <theniphram@gmail.com>
2026-05-14 09:33:42 +0200

    clean up test

--- src/lib/systems/tests/character.test.ts
+++ src/lib/systems/tests/character.test.ts
@@ -1,49 +1,31 @@
-import { glob, readFile } from 'node:fs/promises';
-import { resolve } from 'node:path';
-
-import { describe, expect, test } from 'vitest';
+import { describe, test, vi } from 'vitest';
 
 import { charProxy } from '$lib/serde';
 import { SYSTEMS_MAP } from '$lib/systems';
 
-// Serializer to remove "id" from objects
-expect.addSnapshotSerializer({
-	test(val) {
-		return (
-			// oxlint-disable-next-line typescript/strict-boolean-expressions
-			val
-			&& Object.prototype.hasOwnProperty.call(val, 'id')
-			&& typeof val.id === 'string'
-		);
-	},
-	serialize(val, config, indentation, depth, refs, printer) {
-		delete val.id;
-		return printer(val, config, indentation, depth, refs);
-	},
-});
+// Mock nanoid so generated ids are stable across runs
+vi.mock('nanoid', () => ({
+	nanoid: () => 'static-test-id',
+}));
 
-const testFiles = glob('*.json', { cwd: import.meta.dirname });
+const testFiles = import.meta.glob<{ system: keyof typeof SYSTEMS_MAP }>(
+	'./*.json',
+);
 
 // Snapshot testing for character files
 // Allows manual review if the data model changes
 
 describe('Character Snapshot Tests', async () => {
-	for await (const file of testFiles) {
-		test(file, async () => {
-			const content = (
-				await readFile(resolve(import.meta.dirname, file))
-			).toString();
+	for (const filename in testFiles) {
+		test(filename, async ({ expect }) => {
+			const serialized = await testFiles[filename]();
 
-			const parsed = JSON.parse(content) as { system: string };
+			const deserialized =
+				SYSTEMS_MAP[serialized.system].upgradeCharacterAndDeserialize(
+					serialized,
+				);
 
-			expect(SYSTEMS_MAP).toHaveProperty(parsed.system);
-
-			const newChar =
-				SYSTEMS_MAP[
-					parsed.system as keyof typeof SYSTEMS_MAP
-				].upgradeCharacterAndDeserialize(parsed);
-
-			expect(charProxy(newChar)).toMatchSnapshot();
+			expect(charProxy(deserialized)).toMatchSnapshot();
 		});
 	}
 });
